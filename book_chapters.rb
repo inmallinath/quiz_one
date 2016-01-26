e6b237220b84f6f0f34daaf57a9599767219989c
Mallinath Nagaraj <mallinathin@gmail.com>
2016-01-25 16:07:55 -0800

    Add incomplete book_chapters.rb

---
+++ book_chapters.rb
@@ -0,0 +1,28 @@
+class Book
+  attr_accessor :title, :chapters
+
+  def initialize()
+    @chapters = []
+  end
+
+  def add_chapters(chapter)
+    @chapters << chapter
+  end
+
+  def chapters
+    puts "Your Book: " + @title
+    puts "My awesome book has " + @chapters.length.to_s + " chapters"
+    i = 0
+    @chapters.each { |x| puts i+=1 + ". #{x}" }
+  end
+end
+
+book = Book.new
+book.title = "My Awesome Book"
+book.add_chapters("My Awesome Chapter 1")
+book.add_chapters("My Awesome Chapter 2")
+book.chapters
+# This should print:
+# Your book: My Awesome Book has 2 chapters:
+# 1. My Awesome Chapter 1
+# 2. My Awesome Chapter 2
